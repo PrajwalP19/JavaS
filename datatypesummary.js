5591112a9d20dd3b911b66ce1eca349152834892
ppp2002-19 <ppbaviskar7030@gmail.com>
2024-09-17 13:24:33 +0000

    comparison and datatypes

---
+++ datatypesummary.js
@@ -0,0 +1,45 @@
+// Primitive = (Call by value) 
+// 7 types = string, number, boolean, null, undefined, symbol, BigInt
+
+
+
+// Non-primitive = (call by refrence)
+//  3 types = array, object , function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const id = Symbol ('123')
+const anotherId = ('123')
+console.log(id === anotherId);
+
+
+
+const bigNumber = 7534765308407n 
+
+
+const heros = ["shaktiman","naagraj", "doga"];
+
+let myObj = {
+    name: "Prajwal",
+    age: 23,
+}
+
+ const myFunction = function(){
+    console.log("Hello World");
+    
+ }
