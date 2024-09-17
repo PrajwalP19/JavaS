8620c5ba875f4f6ddaaaf61bbe9b0a38db36a669
ppp2002-19 <ppbaviskar7030@gmail.com>
2024-09-17 11:59:45 +0000

    operations

--- conversionOperations.js
+++ conversionOperations.js
@@ -13,7 +13,44 @@ let isLoggedIn = "prajwal"
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+// operations
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = " Prajwal"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(3+4);
+
+// console.log("1"+ 2+ 3);
+// console.log(2+4+"2");
+
+// console.log(true);
+
+// console.log(+true);
+
+// console.log(true+);  not allowed
+// console.log(+"");
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter
+console.log(gameCounter);
+
+
+
+
+
+
 
 
