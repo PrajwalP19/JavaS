033473025b0632ca9f6efbe9eca04e4ef785f386
ppp2002-19 <ppbaviskar7030@gmail.com>
2024-09-17 18:39:22 +0000

    some changes

--- datatypesummary.js
+++ datatypesummary.js
@@ -1,26 +1,12 @@
 // Primitive = (Call by value) 
 // 7 types = string, number, boolean, null, undefined, symbol, BigInt
 
+const score = 100
+const scoreValue = 100.3 
 
-
-// Non-primitive = (call by refrence)
-//  3 types = array, object , function
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
 
 
 const id = Symbol ('123')
@@ -30,6 +16,12 @@ console.log(id === anotherId);
 
 
 const bigNumber = 7534765308407n 
+
+
+
+// Non-primitive = (call by refrence)
+//  3 types = array, object , function
+
 
 
 const heros = ["shaktiman","naagraj", "doga"];
